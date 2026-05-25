60a75ae58ea8a29148a6058c9923c1125a440678
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2026-05-25 13:18:37 -0400

    Adding migration for user status

---
+++ PayrollBackendProject.Infrastructure/Migrations/20260525171827_AddUserStatusToUsers.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PayrollBackendProject.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserStatusToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AdjustmentCode",
+                table: "PaymentLineItems",
+                newName: "PaymentAdjustmentCode");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserStatus",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserStatus",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "PaymentAdjustmentCode",
+                table: "PaymentLineItems",
+                newName: "AdjustmentCode");
+        }
+    }
+}
